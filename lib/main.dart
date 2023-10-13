59594e5473e38bbfb973d1ca2391f10406f7b49c
shakeerk123 <shakeer.ezhammilee@gmail.com>
2023-10-13 05:27:54 +0000

    commented

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
      // initialRoute: "/home",
      // getPages: appRoutes(),
-      home: LoginScreen(),
+      home: GnavNavigation(),
     );
   }
 }
